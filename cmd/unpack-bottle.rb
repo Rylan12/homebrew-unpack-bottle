74c8a6cbbb616c6ffb245fcacbf092ae241c1d9a
Rylan Polster <rslpolster@gmail.com>
2020-09-13 20:32:17 -0400

    unpack-bottle: add --leave-nested option
    Add --leave-nested option to nest the unpacked bottle in the same
    directory structure it would appear in the Cellar
    (`Cellar/formula/pkg_version/bottle`).
    
    Also now uses `formula.pkg_version` instead of `formula.version` so that
    formulae with revisions can be used.

--- cmd/unpack-bottle.rb
+++ cmd/unpack-bottle.rb
@@ -18,6 +18,8 @@ module Homebrew
       EOS
       flag   "--destdir=",
              description: "Create subdirectories in the directory named by <path> instead."
+      switch "--leave-nested",
+             description: "Leave the unpacked bottle nested in subdirectories as it would be in the Cellar."
       switch "-f", "--force",
              description: "Overwrite the destination directory if it already exists."
 
@@ -42,26 +44,36 @@ module Homebrew
 
     formulae.each do |f|
       if f.bottle.nil?
-        oh1 "No bottles found for #{f.name}-#{f.version}"
+        oh1 "No bottles found for #{f.name}-#{f.pkg_version}"
         next
       end
 
-      stage_dir = unpack_dir/"#{f.name}-#{f.version}"
+      final_dir = unpack_dir/"#{f.name}-#{f.pkg_version}"
 
-      if stage_dir.exist?
-        raise "Destination #{stage_dir} already exists!" unless args.force?
+      if final_dir.exist?
+        raise "Destination #{f.name}-#{f.pkg_version} already exists!" unless args.force?
 
-        rm_rf stage_dir
+        rm_rf final_dir
       end
 
-      oh1 "Unpacking #{Formatter.identifier(f.full_name)} to: #{stage_dir}"
+      oh1 "Unpacking #{Formatter.identifier(f.full_name)} to: #{final_dir}"
 
       bottle_archive = f.bottle.fetch
 
       Utils::Tar.validate_file bottle_archive
 
-      UnpackStrategy.detect(bottle_archive, prioritise_extension: true)
-                    .extract_nestedly(to: stage_dir, prioritise_extension: true, verbose: args.verbose?)
+      Dir.mktmpdir do |tmp_unpack_dir|
+        tmp_unpack_dir = Pathname.new(tmp_unpack_dir)
+
+        UnpackStrategy.detect(bottle_archive, prioritise_extension: true)
+                      .extract_nestedly(to: tmp_unpack_dir, prioritise_extension: true, verbose: args.verbose?)
+
+        if args.leave_nested?
+          cp_r tmp_unpack_dir, final_dir
+        else
+          cp_r tmp_unpack_dir/f.name/f.pkg_version, final_dir
+        end
+      end
     end
   end
 end
